ffaa4237e815735099bd01e7e769523781a248b1
Rifaut Alexis <ralexis149@gmail.com>
2019-10-30 19:06:17 +0100

    init projet

---
+++ Cityton.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Cityton.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ Cityton.Service/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Cityton.Service
+{
+    public class Class1
+    {
+    }
+}
